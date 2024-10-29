8ccebb422fbcef9e882c14b736aed0b69d234d9c
The Bard Who Codes <shaynecodes@gmail.com>
2024-10-29 16:57:55 -0400

    Added door sound

--- Scripts/InteractionSystem/Door.gd
+++ Scripts/InteractionSystem/Door.gd
@@ -2,6 +2,9 @@ extends InteractableObj
 class_name InteractableDoor
 var door_state : bool = false
 @export var anim:AnimatedSprite2D
+@export var audio: AudioStreamPlayer2D
+var rng = RandomNumberGenerator.new()
+
 func _ready() -> void:
 	priority = 2
 	interact()
@@ -12,8 +15,12 @@ func interact() -> void:
 		true:
 			global.pathfinder.set_point_solid(global.map.local_to_map(position), true)
 			anim.play_backwards("open")
+			audio.pitch_scale = rng.randf_range(1, 1.5) # randomize pitch scale to avoid sounding to repetitive
+			audio.play()
 			priority = 2
 		false:
 			global.pathfinder.set_point_solid(global.map.local_to_map(position), false)
 			anim.play("open")
+			audio.pitch_scale = rng.randf_range(1, 1.5) # randomize pitch scale to avoid sounding to repetitive
+			audio.play()
 			priority = 1

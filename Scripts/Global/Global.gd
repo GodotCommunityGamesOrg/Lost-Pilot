f11a813b7d037559d6cc44b6374a4df79030c4c5
elementprogram5 <135732254+elementprogram5@users.noreply.github.com>
2024-10-29 20:56:31 -0400

    Add files via upload

--- Scripts/Global/Global.gd
+++ Scripts/Global/Global.gd
@@ -9,8 +9,6 @@ var pathfinder: AStarGrid2D
 ## Reference to the TileMapLayer node for interacting with the game map.
 var map: TileMapLayer
 
-var player: PlayerNode
-
 ## return A [PackedVector2Array] containing a path as a series of points from the start to the end position.
 ## [param start]: A [Vector2] representing the starting position in the world.
 ## [param end]: A [Vector2] representing the target position in the world.
@@ -21,6 +19,3 @@ func calculate_path(start: Vector2, end: Vector2, tf: bool = true) -> PackedVect
 		map.local_to_map(end),
 		tf
 	)
-
-func in_map(cell: Vector2i):
-	return map.local_to_map(cell) in map.get_used_cells()
---
+++ Scripts/Inventory/PlayerInventory.gd
@@ -0,0 +1,84 @@
+extends Node
+
+signal active_item_updated
+
+const NUM_INVENTORY_SLOTS = 20
+const NUM_HOTBAR_SLOTS = 8
+const ItemClass = preload("res://Objects/Items/Item.gd")
+const SlotClass = preload("res://Scripts/Inventory/Slot.gd")
+
+var inventory = {
+	0 : ["Repair Kit", 5],
+	1 : ["Med Kit", 10],
+	2 : ["Bandage", 30],
+	3 : ["Repair Kit", 2],
+	4 : ["Med Kit", 10],
+	5 : ["Bandage", 30],
+}
+
+var hotbar = {
+	0 : ["Repair Kit", 5],
+	1 : ["Med Kit", 10],
+	2 : ["Bandage", 30],
+	3 : ["Repair Kit", 2],
+	4 : ["Med Kit", 10],
+	5 : ["Bandage", 30],
+}
+
+var active_item_slot = 0
+
+# Called when the node enters the scene tree for the first time.
+func add_item(item_name, item_quantity):
+	for item in inventory:
+		if inventory[item][0] == item_name:
+			var stack_size = int(JsonData.item_data[item_name]["StackSize"])
+			var able_to_add = stack_size - inventory[item][1]
+			if able_to_add >= item_quantity:
+				inventory[item][1] += item_quantity
+				update_slot_visual(item, inventory[item][0], inventory[item][1])
+				return
+			else:
+				inventory[item][1] += able_to_add
+				update_slot_visual(item, inventory[item][0], inventory[item][1])
+				item_quantity = item_quantity - able_to_add
+	for i in range(NUM_INVENTORY_SLOTS):
+		if inventory.has(i) == false:
+			inventory[i] = [item_name, item_quantity]
+			update_slot_visual(i, inventory[i][0], inventory[i][1])
+			return
+		
+func update_slot_visual(slot_index, item_name, new_quantity):
+	var slot = get_tree().root.get_node("/root/World/UI/Inventory/GridContainer/Slot" + str(slot_index + 1))
+	if slot.item != null:
+		slot.item.set_item(item_name, new_quantity)
+	else:
+		slot.initialize_item(item_name, new_quantity)
+
+func remove_item(slot : SlotClass, is_hotbar : bool = false):
+	if is_hotbar:
+		hotbar.erase(slot.slot_index)
+	else:
+		inventory.erase(slot.slot_index)
+			
+func add_item_to_empty_slot(item : ItemClass, slot : SlotClass, is_hotbar : bool = false):
+	if is_hotbar:
+		hotbar[slot.slot_index] = [item.item_name, item.item_quantity]
+	else:
+		inventory[slot.slot_index] = [item.item_name, item.item_quantity]
+	
+func add_item_quantity(slot : SlotClass, quantity_to_add : int, is_hotbar : bool = false):
+	if is_hotbar:
+		hotbar[slot.slot_index][1] += quantity_to_add
+	else:
+		inventory[slot.slot_index][1] += quantity_to_add
+
+func active_item_scroll_up():
+	active_item_slot = (active_item_slot + 1) % NUM_HOTBAR_SLOTS
+	emit_signal("active_item_updated")
+	
+func active_item_scroll_down():
+	if active_item_slot == 0:
+		active_item_slot = NUM_HOTBAR_SLOTS - 1
+	else:
+		active_item_slot -= 1
+	emit_signal("active_item_updated")

5be728e485c43441edf8567dca0ec3a09f6fe766
elementprogram5 <preciouslygames@outlook.com>
2024-12-12 20:55:54 -0500

    Update player_inventory.gd

--- scripts/global/player_inventory.gd
+++ scripts/global/player_inventory.gd
@@ -3,11 +3,15 @@ extends Node
 signal active_item_updated
 signal inventory_active_item_updated
 
+# Constants defining the number of inventory and hotbar slots
 const NUM_INVENTORY_SLOTS = 20
 const NUM_HOTBAR_SLOTS = 8
+
+# Preloaded classes for Item and Slot
 const ItemClass = preload("res://scripts/ui/inventory/item.gd")
 const SlotClass = preload("res://scripts/ui/inventory/slot.gd")
 
+# Player Inventory Dictionary
 var inventory = {
 	0 : ["Repair Kit", 5],
 	1 : ["Med Kit", 10],
@@ -21,6 +25,7 @@ var inventory = {
 	9 : ["Intermediate Helmet", 1],
 }
 
+# Player Hotbar Dictionary
 var hotbar = {
 	0 : ["Repair Kit", 5],
 	1 : ["Med Kit", 10],
@@ -30,16 +35,17 @@ var hotbar = {
 	5 : ["Bandage", 20],
 }
 
+# Player Equip Dictionary
 var equips = {
 	0 : ["Basic Helmet", 1],
 	1 : ["Basic Vest", 1],
 	2 : ["Basic Boots", 1],
 	3 : ["Weapon 1", 1],
 }
-
+# Keeps track of index of selected slots
 var active_item_slot = 0
 
-# Called when the node enters the scene tree for the first time.
+# Adds items to the inventory.
 func add_item(item_name, item_quantity):
 	for item in inventory:
 		if inventory[item][0] == item_name:
@@ -59,6 +65,7 @@ func add_item(item_name, item_quantity):
 			update_slot_visual(i, inventory[i][0], inventory[i][1])
 			return
 		
+# Updates the visual inventory
 func update_slot_visual(slot_index, item_name, new_quantity):
 	var slot = get_tree().root.get_node("/root/World/UI/Inventory/GridContainer/Slot" + str(slot_index + 1))
 	if slot.item != null:
@@ -66,6 +73,7 @@ func update_slot_visual(slot_index, item_name, new_quantity):
 	else:
 		slot.initialize_item(item_name, new_quantity)
 
+# Removes items from the inventory.
 func remove_item(slot : SlotClass):
 	match slot.SlotType:
 		SlotClass.SlotType.HOTBAR:
@@ -74,7 +82,8 @@ func remove_item(slot : SlotClass):
 			inventory.erase(slot.slot_index)
 		_:
 			equips.erase(slot.slot_index)
-	
+			
+# Adds items to empty slots in the inventory.
 func add_item_to_empty_slot(item : ItemClass, slot : SlotClass):
 	match slot.SlotType:
 		SlotClass.SlotType.HOTBAR:
@@ -83,7 +92,8 @@ func add_item_to_empty_slot(item : ItemClass, slot : SlotClass):
 			inventory[slot.slot_index] = [item.item_name, item.item_quantity]
 		_:
 			equips[slot.slot_index] = [item.item_name, item.item_quantity]
-	
+
+# Adds quantity to items in the inventory
 func add_item_quantity(slot : SlotClass, quantity_to_add : int):
 	match slot.SlotType:
 		SlotClass.SlotType.HOTBAR:
@@ -91,17 +101,20 @@ func add_item_quantity(slot : SlotClass, quantity_to_add : int):
 		SlotClass.SlotType.INVENTORY:
 			inventory[slot.slot_index][1] += quantity_to_add
 
+# Removes quantity from items in the inventory
 func decrease_item_quantity(slot : SlotClass, quantity_to_remove : int):
 	match slot.SlotType:
 		SlotClass.SlotType.HOTBAR:
 			hotbar[slot.slot_index][1] -= quantity_to_remove
 		SlotClass.SlotType.INVENTORY:
 			inventory[slot.slot_index][1] -= quantity_to_remove
-			
+
+# Changes the active item slot in the hotbar and emit a signal
 func change_active_item_slot(active_slot):
 	active_item_slot = active_slot
 	emit_signal("active_item_updated")
-	
+
+# Changes the active item slot in the inventory and emit a signal
 func change_inventory_active_item_slot(active_slot):
 	active_item_slot = active_slot
 	emit_signal("inventory_active_item_updated")

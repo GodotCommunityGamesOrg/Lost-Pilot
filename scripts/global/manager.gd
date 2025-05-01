3a32a96fbb15ea683acf881ba4097f33ebc53573
ChaoticTechSupport <ChaoticTech0111@gmail.com>
2025-05-01 14:20:11 -0400

    - final update

--- scripts/global/manager.gd
+++ scripts/global/manager.gd
@@ -1,32 +1,66 @@
 extends Node
 class_name Manager
-# GameManager that handles loading and spawning scenes
-# e.g Player, world scene and more.
-
-# This script will evolve as the game progresses.
+## GameManager that handles loading and spawning scenes
+## e.g Player, world scene and more.
 
 
-# Add to the keys for specific scenes.
-enum Keys {SpaceStation,ShipEditor,SpaceEnvironment, main}
+const loading_scene: String = "res://scenes/loader.tscn" ## path to the loading screen
 
-# Store the scenes
-var scenes : Dictionary
+var loader: ProgressBar ## loading screen progress
 
-var current_scene : Node
-var scene_container : Node2D
+## keys for each level that corrisponds with the scene path.
+enum Keys {
+	## SpaceStation level where most of the playing takes place.
+	SpaceStation,
+	## Space Ship (player home)
+	ShipEditor, 
+	## ship Navigation area
+	SpaceEnvironment,
+	## Main Menu 
+	main 
+	}
+
+var scenes : Dictionary ## Scene path storage
+var current_scene : Node ## the Main Scene currently loaded
+
+var _load_key : int ## which scene loading (do not touch)
+
 func _ready() -> void:
-	add_scene("res://scenes/player/world.tscn",Keys.SpaceStation)
+	add_scene("res://scenes/player/world.tscn", Keys.SpaceStation)
 	add_scene("res://scenes/main/main.tscn", Keys.main)
+	add_child(preload(loading_scene).instantiate())
+	loader = get_child(0).get_child(0)
+	spawn_scene(Keys.main)
 
-# Add scenes to the scene dict.
-func add_scene(scene : String,key : Keys) -> void:
+## adds scene path to dictionary with assignes key
+func add_scene(scene : String, key : Keys) -> void:
 	scenes[key] = scene
 
+## deletes primary scene
 func delete_scene() -> void:
 	if current_scene:
 		current_scene.queue_free()
+		current_scene = null
 
-# Spawns the scene to the tree.
-func spawn_scene(key : Keys) -> void:
-	
-	get_tree().change_scene_to_file(scenes[key])
+## replaces scene with new scene.
+func spawn_scene(key : Keys, function_await: Callable = func() -> void: pass) -> void:
+	if !scenes.has(key):
+		return
+	_load_key = key
+	var path = scenes[key]
+	ResourceLoader.load_threaded_request(path)
+	while true:
+		var total = []
+		var stage = ResourceLoader.load_threaded_get_status(scenes[_load_key], total)
+		loader.value = total[0] * 100.0
+
+		if stage == ResourceLoader.ThreadLoadStatus.THREAD_LOAD_LOADED and total[0] == 1.0:
+			var res = ResourceLoader.load_threaded_get(scenes[_load_key])
+			if res:
+				delete_scene()
+				current_scene = res.instantiate()
+				add_child(current_scene)
+			await function_await.call()
+			loader.get_parent().visible = false
+			break
+		await get_tree().process_frame
--- scripts/menu_system/menu_system.gd
+++ scripts/menu_system/menu_system.gd
@@ -3,24 +3,25 @@ class_name MenuSystem
 
 
 
-## Add to the keys enum when adding a new menu.
-## Stores all the menus in memory and stored in a dict using the keys enum as input.
-var menus : Dictionary = {}
+
 ## Current menu that is being shown.
 var current_menu: MenuBase
+
 ## Storing a single array of menus so we can go back a menu without the new menu knowing it.
 var previous_menus: Array[int] = []
-## Where menus are instanced into the scene.
 
 func _ready() -> void:
 	tabs_visible = false
 	for i: MenuBase in get_children():
 		i.set_menu_manager(self) 
-## Load a previous menu using the previous_menus array we pop the current menu
+
+## switch to the previous menu using the previous_menus array we pop the current menu
 ## so we get the previous menu
 func switch_to_previous_menu() -> void:
 	## Delete the last menu in the previous menu array and load it
 	switch_menu(previous_menus.pop_back())
+
+## switch to the menu with the given id
 func switch_menu(id: int):
 	previous_menus.append(current_tab)
 	current_tab = id;

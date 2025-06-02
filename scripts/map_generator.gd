43f6302783287e938983f9627e073a47b988da15
elementprogram5 <preciouslygames@outlook.com>
2025-06-02 16:48:59 -0400

    Fixed movement error

--- scripts/map_generator.gd
+++ scripts/map_generator.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 	WorldPathfinder.pathfinder = AStarGrid2D.new()
 	WorldPathfinder.pathfinder.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
 	WorldPathfinder.pathfinder.cell_size = Vector2.ONE * 128
-	WorldPathfinder.pathfinder.region = get_used_rect()
+	WorldPathfinder.pathfinder.region = Rect2i(0, 0, get_parent().room_width, get_parent().room_height)
 	WorldPathfinder.pathfinder.update()
 	for pos in get_used_cells():
 		if get_cell_tile_data(pos).get_custom_data("Col") == 0:
